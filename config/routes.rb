27cb23a9b718c4dbde0d87c33cf135fb184d04c6
Cristian Goenaga <crisgoera@gmail.com>
2023-08-14 13:02:03 +0200

    Daily challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get '/contact', to: 'pages#contact'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
